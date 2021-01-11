419dbe35ef40c6c528129b663435ac2542a55fdc
zhangshaotian <746192615@qq.com>
2021-01-11 18:02:12 +0800

    modified

--- lodash/astarlessnight-lodash.js
+++ lodash/astarlessnight-lodash.js
@@ -1,18 +1,19 @@
 
 
-var astarlessnight = function() {
-    function chunk (array, size = 1) {
-        let result = []
-        let start = 0
+var astarlessnight = function () {
+    function chunk(array, size = 1) {
+        let result = new Array(Math.ceil(array.length / size))
         let count = (array.length / size) | 0
+        let idx = 0
+        let start = 0
 
         for (let i = 1; i <= count; i++) {
-            result.push(array.slice(start, start + size))
+            result[idx++] = array.slice(start, start + size)
             start = start + size
         }
 
         if (start != array.length) {
-            result.push(array.slice(start))
+            result[idx++] = array.slice(start)
         }
 
         return result
@@ -22,3 +23,8 @@ var astarlessnight = function() {
         chunk,
     }
 }()
+
+
+console.log(astarlessnight.chunk(["0", "1", "2", "3", "4", "5"], 2))
+console.log(astarlessnight.chunk(["0", "1", "2", "3", "4", "5"], 3))
+console.log(astarlessnight.chunk(["0", "1", "2", "3", "4", "5"], 4))
